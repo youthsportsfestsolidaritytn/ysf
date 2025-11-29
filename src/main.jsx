49a723aae5f282e78e22d09a0d21a53d5c21678a
Salmankhan A <146570509+Salmankhan-A@users.noreply.github.com>
2025-11-29 17:58:25 +0530

    Screen size decreased

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import AppRouter from "./routes/AppRoutes";
-import "./styles/index.css";
+import "./styles/global.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
